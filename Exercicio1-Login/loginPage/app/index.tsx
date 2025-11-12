8ee72e7c78dddfbf74dfcc25962886921e623cfe
BecaIanz <silveiraianzr@gmail.com>
2025-11-12 11:30:29 -0300

    terminei o cadastro

--- Exercicio1-Login/loginPage/app/index.tsx
+++ Exercicio1-Login/loginPage/app/index.tsx
@@ -1,29 +1,241 @@
-import { Platform, StyleSheet, Text, View, Image, TextInput } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, TextInput, TouchableOpacity } from 'react-native';
 import {useState} from 'react';
 
 export default function HomeScreen() {
+
   return (
-    <View style={{flex:1, backgroundColor: "#EAD6FF", justifyContent: "flex-start"}}>
-      <View style={{flex:1, height:200, alignItems:"center",justifyContent:'flex-start'}}>
-        <Text style={styles.text}>Welcome!</Text>
+    <View style={styles.container }>
+      <View style={styles.nav}>
+        <Text style={styles.text}>Cadastro</Text>
+      </View>
+      <View style={styles.body}>
+
         <TextInput placeholder='Email' style={styles.input}></TextInput>
+        <TextInput secureTextEntry={true} placeholder='Senha' style={styles.input}></TextInput>
+        <TextInput secureTextEntry={true} placeholder='Confirmar Senha' style={styles.input}></TextInput>
+ 
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.textbutton}>Cadastrar</Text>
+        </TouchableOpacity>
+
+      </View>
+      <View style={styles.final}>
+        <TouchableOpacity style={styles.button2}>
+          <Image  source={require("../assets/images/googlepequeno.png")}></Image>
+          <Text style={{fontSize: 25, fontFamily: 'MyCustomFontName', color: " "}}>Google</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.button3}>
+          <Image  source={require("../assets/images/facebook40.png")}></Image>
+          <Text style={{fontSize: 25, fontFamily: 'MyCustomFontName', color: " "}}>Facebook</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container : {
+    flex:1,
+    backgroundColor: "#EAD6FF", 
+    justifyContent: "flex-start",
+    gap : 20
+  },
+  nav:{
+    width: "100%",
+    height : "20%",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"
+
+  },
+  body:{
+    width: "100%",
+    height : "40%",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    gap : 20  
+  },
+  final:{
+    width: "100%",
+    height : "40%",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center",
+    gap : 20,
+    flexDirection: "column"
+  },
   text : {
-    color: "#A851FC",
+    color: "rebeccapurple",
     fontSize: 50,
-    fontFamily: 'MyCustomFontName'
+    fontFamily: 'MyCustomFontName',
+  },
+  img : {
+    width: "40%",
+    height: "100%"
   },
   input: {
     backgroundColor: '#F5EBFF',
     width: 300,
-    height: 35,
+    height: 45,
     borderRadius: 10,
     padding: 10,
     color: '#B4B1BD'
+  },
+  button : {
+    backgroundColor: "rebeccapurple",
+    width: 300,
+    height: 45,
+    borderRadius: 10,
+    padding: 10,
+    display:"flex",
+    justifyContent:"center",
+    alignItems: "center"
+  },
+  button2 : {
+    width: 300,
+    height: 20,
+    display:"flex",
+    justifyContent:"center",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  button3 : {
+    width: 80,
+    height: 20,
+    display:"flex",
+    justifyContent:"center",
+    alignItems: "center",
+    flexDirection: "row"
+  },
+  textbutton :{
+    fontSize: 20,
+    fontFamily: 'MyCustomFontName',
+    color: "white"
+  },
+  textbutton2 :{
+    fontSize: 15,
+    fontFamily: 'MyCustomFontName',
+    color: "rebeccapurple"
   }
+//   return (
+//     <View style={styles.container }>
+//       <View style={styles.nav}>
+//         <Text style={styles.text}>Welcome!</Text>
+//       </View>
+//       <View style={styles.body}>
+
+//         <TextInput placeholder='Email' style={styles.input}></TextInput>
+//         <TextInput secureTextEntry={true} placeholder='Senha' style={styles.input}></TextInput>
+ 
+//         <TouchableOpacity style={styles.button}>
+//           <Text style={styles.textbutton}>Login</Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity style={styles.button2}>
+//           <Text style={styles.textbutton2}>Forgot Password?</Text>
+//         </TouchableOpacity>
+
+//       </View>
+//       <View style={styles.final}>
+//         <TouchableOpacity style={styles.button2}>
+//           <Image  source={require("../assets/images/googlepequeno.png")}></Image>
+//           <Text style={{fontSize: 25, fontFamily: 'MyCustomFontName', color: " "}}>Google</Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity style={styles.button3}>
+//           <Image  source={require("../assets/images/facebook40.png")}></Image>
+//           <Text style={{fontSize: 25, fontFamily: 'MyCustomFontName', color: " "}}>Facebook</Text>
+//         </TouchableOpacity>
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container : {
+//     flex:1,
+//     backgroundColor: "#EAD6FF", 
+//     justifyContent: "flex-start",
+//     gap : 20
+//   },
+//   nav:{
+//     width: "100%",
+//     height : "20%",
+//     display:"flex",
+//     justifyContent:"center",
+//     alignItems:"center"
+
+//   },
+//   body:{
+//     width: "100%",
+//     height : "40%",
+//     display:"flex",
+//     justifyContent:"center",
+//     alignItems:"center",
+//     gap : 20  
+//   },
+//   final:{
+//     width: "100%",
+//     height : "40%",
+//     display:"flex",
+//     justifyContent:"center",
+//     alignItems:"center",
+//     gap : 20,
+//     flexDirection: "column"
+//   },
+//   text : {
+//     color: "rebeccapurple",
+//     fontSize: 50,
+//     fontFamily: 'MyCustomFontName',
+//   },
+//   img : {
+//     width: "40%",
+//     height: "100%"
+//   },
+//   input: {
+//     backgroundColor: '#F5EBFF',
+//     width: 300,
+//     height: 45,
+//     borderRadius: 10,
+//     padding: 10,
+//     color: '#B4B1BD'
+//   },
+//   button : {
+//     backgroundColor: "rebeccapurple",
+//     width: 300,
+//     height: 45,
+//     borderRadius: 10,
+//     padding: 10,
+//     display:"flex",
+//     justifyContent:"center",
+//     alignItems: "center"
+//   },
+//   button2 : {
+//     width: 300,
+//     height: 20,
+//     display:"flex",
+//     justifyContent:"center",
+//     alignItems: "center",
+//     flexDirection: "row"
+//   },
+//   button3 : {
+//     width: 80,
+//     height: 20,
+//     display:"flex",
+//     justifyContent:"center",
+//     alignItems: "center",
+//     flexDirection: "row"
+//   },
+//   textbutton :{
+//     fontSize: 20,
+//     fontFamily: 'MyCustomFontName',
+//     color: "white"
+//   },
+//   textbutton2 :{
+//     fontSize: 15,
+//     fontFamily: 'MyCustomFontName',
+//     color: "rebeccapurple"
+//   }
 });
